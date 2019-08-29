a0471838a27243a6846d36062e0fca1df715cb91
viernes <luis.muro@gmail.com>
2019-08-29 12:18:45 -0500

    Se agregaron campos check y ocultaron campos

--- info_students/models/models.py
+++ info_students/models/models.py
@@ -10,3 +10,9 @@ class StudentsFields(models.Model):
     birth_certificate = fields.Binary(string="Acta de nacimiento")
     curp = fields.Binary(string="CURP")
     highschool_certificate = fields.Binary(string="Certificado de secundaria")
+
+    check_identification_oficial = fields.Boolean( string = "Identificación oficial", default = False )
+    check_adress_proof = fields.Boolean( string = "Comprobante de domicilio", default = False )
+    check_birth_certificate = fields.Boolean( string ="Acta de nacimiento", default = False )
+    check_curp = fields.Boolean( string = "CURP", default = False )
+    check_highschool_certificate = fields.Boolean( string = "Certificado de secundaria", default = False )
